2601a6629ad4bf88f356a63f4980617e9b85b505
teppei <ushizawa@vaile.co.jp>
2022-10-26 17:33:01 +0900

    add introduction create new files
    新規ファイルを追加
    メインクラスにイントロダクションを追加

---
+++ src/housekeeping_book/HousekeepingBook.java
@@ -0,0 +1,24 @@
+package housekeeping_book;
+
+/**
+ * メインクラス
+ * ユーザーにメニューから閲覧、登録、更新、削除を選択させ
+ * それぞれの処理を行う
+ *
+ * @author ushizawa.teppei
+ */
+
+public class HousekeepingBook {
+
+	public static void main(String[] args) {
+		System.out.println("-------------------------------");
+		System.out.println("|　家計簿システムへようこそ　 |");
+		System.out.println("-------------------------------");
+		System.out.println("");
+		System.out.println("以下のメニューから選択してください");
+		System.out.println("1.閲覧　2.登録　3.更新　4.削除");
+
+
+	}
+
+}
